e85b766aa55500b5b94b7eee85554ca6fc241591
banu-i <152266442+banu-i@users.noreply.github.com>
2024-07-24 14:57:32 +0200

    20240724 courseAssignment
    Notiz:
    terraform: init | plan |apply | (destroy)
    Sicherheitsgruppen: SSH

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
-provider "" {
-  
+provider "aws" {
+#   hier kommen die interesaanten Werte
+region = "eu-central-1"
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "tolle-ec2" {
+  instance_type =  "t2.micro"
+  ami = "ami-071878317c449ae48"
+  vpc_security_group_ids =  ["sg-0e703645fb1ff55b7"]
+  key_name = "my-instance-1_key"
 }
